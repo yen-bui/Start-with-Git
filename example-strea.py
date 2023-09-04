83ef07818a5688acce6d0db635a85dfcbaf7e713
yen-bui <137190230+yen-bui@users.noreply.github.com>
2023-09-04 14:17:07 +0200

    Update example-strea.py

--- example-strea.py
+++ example-strea.py
@@ -7,11 +7,11 @@ def page1():
     # Display the Plotly graph using an IFrame
     st.write("Plotly Graph on Page 1:")
     
-    # Provide the URL to the raw GitHub content of your HTML file
-    github_raw_url = "https://drive.google.com/uc?id=1chj9-22F3NPzwEOsonnAT5txamawookM"
+    # Provide the direct link to the Google Drive file
+    google_drive_url = "https://drive.google.com/file/d/1chj9-22F3NPzwEOsonnAT5txamawookM/preview"
     
-    # Use the IFrame component to display the external HTML content
-    st.components.v1.html(f'<iframe src="{github_raw_url}" width="1000" height="600"></iframe>', width=1000, height=600, scrolling=True)
+    # Use the iframe tag to display the external content
+    st.write(f'<iframe src="{google_drive_url}" width="1000" height="600"></iframe>', unsafe_allow_html=True)
     
 def page2():
     st.title("Page 2")
