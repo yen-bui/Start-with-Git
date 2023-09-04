e59cb77d4d0e107c9846bc84e90641e147261a65
yen-bui <137190230+yen-bui@users.noreply.github.com>
2023-09-04 13:58:53 +0200

    Update example-strea.py

--- example-strea.py
+++ example-strea.py
@@ -1,23 +1,17 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
-
-
-# Create a function to display the Plotly graph
-def display_plotly_graph():
-    # Load the Plotly HTML file
-    with open("plotly_graph.html", "r") as file:
-        plotly_html = file.read()
-
-    # Display the Plotly graph using st.write()
-    st.write(plotly_html, unsafe_allow_html=True)
 
 def page1():
     st.title("Page 1")
     st.write("Welcome to Page 1!")
 
-    # Display the Plotly graph on Page 1
-    display_plotly_graph()
+    # Display the Plotly graph using an IFrame
+    st.write("Plotly Graph on Page 1:")
+    
+    # Provide the URL to your Plotly graph hosted externally (e.g., GitHub Gist, Google Drive, etc.)
+    plotly_graph_url = "https://gist.githubusercontent.com/your_username/gist_id/raw/plotly_graph.html"
+    
+    # Use the IFrame component to display the external HTML content
+    st.components.v1.html(f'<iframe src="{plotly_graph_url}" width="1000" height="600"></iframe>', width=1000, height=600, scrolling=True)
 
 def page2():
     st.title("Page 2")
