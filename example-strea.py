1a677e2caf687c2a480c832331633273d82bf443
yen-bui <137190230+yen-bui@users.noreply.github.com>
2023-09-04 14:05:11 +0200

    Update example-strea.py

--- example-strea.py
+++ example-strea.py
@@ -7,13 +7,12 @@ def page1():
     # Display the Plotly graph using an IFrame
     st.write("Plotly Graph on Page 1:")
     
-    # Provide the URL to your Plotly graph hosted externally (e.g., GitHub Gist, Google Drive, etc.)
-    plotly_graph_url = "https://github.com/yen-bui/Start-with-Git/blob/main/plotly_graph.html"
-    
+    # Provide the URL to the raw GitHub content of your HTML file
+    github_raw_url = "https://raw.githubusercontent.com/yen-bui/Start-with-Git/main/plotly_graph.html"
     
     # Use the IFrame component to display the external HTML content
-    st.components.v1.html(f'<iframe src="{plotly_graph_url}" width="1000" height="600"></iframe>', width=1000, height=600, scrolling=True)
-
+    st.components.v1.html(f'<iframe src="{github_raw_url}" width="1000" height="600"></iframe>', width=1000, height=600, scrolling=True)
+    
 def page2():
     st.title("Page 2")
     st.write("Welcome to Page 2!")
